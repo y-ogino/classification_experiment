0bdee5d9a3a1d49a3b7079f554d92eb5c49e89f5
Yuto Ogino <ogino@ykw.elec.keio.ac.jp>
2018-12-28 16:51:58 +0900

    test multiple tree-based methods in `main.py`

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from dataset import ClassificationDataset
-from evaluation import auc, confusion_matrix, f1_score
-from sklearn.tree import DecisionTreeClassifier
+import evaluation as evl
 import pandas as pd
-import matplotlib.pyplot as plt
+import sklearn.ensemble as ens
+from sklearn.tree import DecisionTreeClassifier as tree
+
+classifiers = [
+    tree(min_samples_split = 100),
+    tree(min_samples_split = 40),
+    tree(min_samples_split = 30),
+    tree(min_samples_split = 20),
+    tree(min_samples_split = 10),
+    ens.AdaBoostClassifier(),
+    ens.BaggingClassifier(),
+    ens.ExtraTreesClassifier(),
+    ens.GradientBoostingClassifier(),
+    ens.RandomForestClassifier()
+]
 
 df   = pd.read_csv("../data/train+test.csv").replace({'yes': 1, 'no': 0})
 data = ClassificationDataset.from_dataframe(df)
 
-classifier = DecisionTreeClassifier(min_samples_split = 20)
-clf        = data.train(classifier)
-
-auc(data, clf)
-confusion_matrix(data, clf)
-f1_score(data, clf)
+aucs = [evl.auc(data, data.train(classifier)) for classifier in classifiers]
+aucs
