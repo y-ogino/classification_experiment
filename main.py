9795f8833cba211ad662611ab6fb3b5a68a95375
ogino <ogino@ykw.elec.keio.ac.jp>
2018-12-28 20:44:29 +0900

    print AUC and F-measure

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from dataset import LabeledDataset
 from classifiers import classifiers
 import evaluation as evl
@@ -5,9 +6,11 @@ import pandas as pd
 
 def test(data, classifier):
     clf = data.train(classifier)
-    return evl.auc(data, clf)
+    auc = evl.auc(data, clf)
+    f1  = evl.f1_score(data, clf)
+    return {"AUC": auc, "F-measure": f1}
 
-df   = pd.read_csv("../data/train+test.csv").replace({'yes': 1, 'no': 0})
-data = LabeledDataset.from_dataframe(df)
-aucs = [test(data, classifier) for classifier in classifiers]
-aucs
+df     = pd.read_csv("../data/train+test.csv").replace({'yes': 1, 'no': 0})
+data   = LabeledDataset.from_dataframe(df)
+scores = [test(data, classifier) for classifier in classifiers]
+pprint(scores)
