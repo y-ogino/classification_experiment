cb4dceb12d3b4693648b39f6f9f715a6fdb7268b
ogino <ogino@ykw.elec.keio.ac.jp>
2019-01-05 13:50:16 +0900

    add `if __name__ == "__main__"`

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ def test(data, classifier):
     f1  = evl.f1_score(data, clf)
     return {"AUC": auc, "F-measure": f1}
 
-df     = pd.read_csv("../data/train+test.csv").replace({'yes': 1, 'no': 0})
-data   = LabeledDataset.from_dataframe(df)
-scores = [test(data, classifier) for classifier in classifiers]
+if __name__ == "__main__":
+    df     = pd.read_csv("../data/train+test.csv").replace({'yes': 1, 'no': 0})
+    data   = LabeledDataset.from_dataframe(df)
+    scores = [test(data, classifier) for classifier in classifiers]
 
-scores_df = pd.DataFrame(data = scores, index = classifier_names)
-print(scores_df)
+    scores_df = pd.DataFrame(data = scores, index = classifier_names)
+    print(scores_df)
